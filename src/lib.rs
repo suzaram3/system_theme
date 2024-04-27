e05dfbc541baa2134facc82e51783c60a66f76e1
Mitch Suzara <suzaram3@gmail.com>
2024-04-27 10:54:48 -0400

    Add cli args parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,36 @@
 use std::fs::File;
 use std::io::{self, Read};
+use toml::Value;
+
+#[derive(Debug)]
+pub struct Config {
+    pub kdeglobals_path: String,
+    pub pattern: String,
+}
+
+impl Config {
+    pub fn from_file(file_path: &str) -> Result<Self, Box<dyn std::error::Error>> {
+        let mut file = File::open(file_path)?;
+        let mut toml_str = String::new();
+        file.read_to_string(&mut toml_str)?;
+
+        let toml_value: Value = toml::from_str(&toml_str)?;
+        let kdeglobals_path = toml_value["kde_config"]["kdeglobals_path"]
+            .as_str()
+            .ok_or("kdegloblas_path not found in config file")?
+            .to_string();
+        let pattern = toml_value["kde_config"]["pattern"]
+            .as_str()
+            .ok_or("pattern not found in config file")?
+            .to_string()
+            .to_lowercase();
+
+        Ok(Config {
+            kdeglobals_path,
+            pattern,
+        })
+    }
+}
 
 pub fn list_current_theme(file_path: &str, pattern: &str) -> Result<String, io::Error> {
     let mut file = File::open(file_path)?;
@@ -14,4 +45,31 @@ pub fn list_current_theme(file_path: &str, pattern: &str) -> Result<String, io::
         }
     }
     Ok(current_theme)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_list_current_theme() {
+        let temp_file_path = "test_file.txt";
+        let temp_file_content = "Some line\nLookAndFeelPackage=ThemeName\nAnother line";
+        std::fs::write(temp_file_path, temp_file_content).expect("Failed to write temp file");
+
+        let result = list_current_theme(temp_file_path, "lookandfeelpackage").unwrap();
+
+        assert_eq!(result, "ThemeName".to_string());
+
+        std::fs::remove_file(temp_file_path).expect("Failed to remove temp file");
+    }
+
+    #[test]
+    fn test_list_current_theme_light() {
+        let temp_file_path = "/home/rinzler/.config/kdeglobals";
+
+        let result = list_current_theme(temp_file_path, "lookandfeelpackage").unwrap();
+
+        assert_eq!(result, "org.manjaro.breath-light.desktop".to_string());
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,34 @@
-use system_theme::{self, list_current_theme};
+use clap::{App, AppSettings, Arg};
+use system_theme::*;
 
 fn main() {
-    let home_dir = std::env::var("HOME").expect("HOME var not set.");
-    let kdeglobals_path = format!("{}/.config/kdeglobals", home_dir);
-    let pattern = "lookandfeelpackage";
+    let config = Config::from_file("config.toml").expect("Failed to load config");
 
-    let contents = list_current_theme(&kdeglobals_path, &pattern);
+    let matches = App::new("System Theme")
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .arg(
+            Arg::with_name("list")
+                .short('l')
+                .long("list")
+                .value_name("LIST")
+                .help("List the current system theme")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("toggle")
+                .short('t')
+                .long("toggle")
+                .value_name("TOGGLE")
+                .help("Toggle the current system light/dark")
+                .takes_value(false),
+        )
+        .get_matches();
 
-    print!("Contents: {:?}", contents);
+    if matches.is_present("list") {
+        let contents = list_current_theme(&config.kdeglobals_path, &config.pattern);
+        match contents {
+            Ok(theme) => println!("Current system theme: {}", theme),
+            Err(err) => eprint!("Error: {}", err),
+        }
+    }
 }
